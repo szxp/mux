193183f89cd91aa8f1e949e4c50d5337dc6fd6d5
Péter Szakszon <szp0530@gmail.com>
2017-01-20 19:24:50 +0100

    update doc

--- mux.go
+++ mux.go
@@ -1,4 +1,33 @@
 // Package mux provides a lightweight HTTP request router (multiplexer).
+//
+// Example:
+//   package main
+//
+//   import (
+//       "fmt"
+//       "github.com/szxp/mux"
+//       "net/http"
+//   )
+//
+//   func main() {
+//       muxer := mux.NewMuxer()
+//       muxer.HandleFunc("/", indexHandler, "GET")
+//       muxer.HandleFunc("/login", loginHandler, "GET", "POST")
+//       muxer.HandleFunc("/users/:username", userHandler)
+//       http.ListenAndServe(":8080", muxer)
+//   }
+//
+//   func indexHandler(w http.ResponseWriter, r *http.Request) {
+//       fmt.Fprint(w, "Home")
+//   }
+//
+//   func loginHandler(w http.ResponseWriter, r *http.Request) {
+//       fmt.Fprintf(w, "%s %s", r.Method, "Login")
+//   }
+//
+//   func userHandler(w http.ResponseWriter, r *http.Request) {
+//       fmt.Fprint(w, r.Context().Value(mux.CtxKey("username")))
+//   }
 package mux
 
 import (
