fc41699852bc8afe298266e0bf9f5a2e41e8ef7e
Péter Szakszon <szp0530@gmail.com>
2017-02-23 18:53:01 +0100

    Update mux.go

--- mux.go
+++ mux.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Szakszon Péter. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package mux provides a lightweight HTTP request router (multiplexer).
 //
 // Example:
